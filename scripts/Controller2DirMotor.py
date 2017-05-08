46b4386a794f228b00d2cce7fabee986cd08526d
Tachikoma Dev <tachikoma@cyve.co>
2017-05-08 16:47:13 -0600

    more robust control

--- scripts/Controller2DirMotor.py
+++ scripts/Controller2DirMotor.py
@@ -10,37 +10,44 @@ from ros_pololu_servo.msg import MotorState as MS
 from ros_pololu_servo.msg import MotorStateList as MSL
 from ros_pololu_servo.msg import MotorCommand as MC
 
+PUBTHRESH = 0.01 # don't bother publishing if the last publish is within this percent
+
 class CTLR_SUB:
     def __init__(self):
         self.steer_sub = rospy.Subscriber('steering/control_effort', Float64, self.steer_listen)
         self.throttle_sub = rospy.Subscriber('throttle/control_effort', Float64,
                                              self.throttle_listen)
-        self.steer_pub= rospy.Publisher('/pololu/command/', MC, queue_size = 1)
-        self.throttle_pub= rospy.Publisher('/pololu/command/', MC, queue_size = 1)
+        self.motor_pub= rospy.Publisher('/pololu/command/', MC, queue_size = 3)
+        self.steer_last = 0.0
+        self.throttle_last = 0.0
 
     def steer_listen(self, msg):
         '''string joint_name       # Name of the joint (specified in the yaml file), or motor_id for default calibration
         float64 position        # Position to move to in radians
         float32 speed           # Speed to move at (0.0 - 1.0)
         float32 acceleration    # Acceleration to move at (0.0 - 1.0)'''
-        output = MC()
-        output.joint_name = 'Turning'
-        output.position = msg.data
-        output.speed = 1.0
-        output.acceleration = 0.3
-        self.steer_pub.publish(output)
+        if abs(self.steer_last-msg.data)/(abs(msg.data)+0.001) > PUBTHRESH:
+            output = MC()
+            output.joint_name = 'Turning'
+            output.position = msg.data
+            output.speed = 1.0
+            output.acceleration = 0.3
+            self.motor_pub.publish(output)
+            self.steer_last = msg.data
 
     def throttle_listen(self, msg):
         '''string joint_name       # Name of the joint (specified in the yaml file), or motor_id for default calibration
         float64 position        # Position to move to in radians
         float32 speed           # Speed to move at (0.0 - 1.0)
         float32 acceleration    # Acceleration to move at (0.0 - 1.0)'''
-        output = MC()
-        output.joint_name = 'Throttle'
-        output.position = msg.data
-        output.speed = 1.0
-        output.acceleration = 0.5
-        self.throttle_pub.publish(output)
+        if abs(self.throttle_last-msg.data)/(abs(msg.data)+0.001) > PUBTHRESH:
+            output = MC()
+            output.joint_name = 'Throttle'
+            output.position = msg.data
+            output.speed = 1.0
+            output.acceleration = 0.5
+            self.motor_pub.publish(output)
+            self.throttle_last = msg.data
 
 def main():
     rospy.init_node('turning_motor')
--- scripts/DualSensor2DirController.py
+++ scripts/DualSensor2DirController.py
@@ -59,6 +59,12 @@ HISTORY = 4 # smooths responses, but also slows reaction, needs to be > 1
 BUFFER = 12 # targeted distance to avoid in inches
 STEERING_SENSITIVITY = 0.5 # rescaling factor
 
+MINMOVE_ITERATIONS = 20 #
+MINMOVE            = 0.05 # fractional amount
+THROTTLE_SENSITIVITY = 0.05 # rescaling factor
+KILL_SOLO          = 25
+KILL_DUAL          = 40
+
 class SteeringDecider:
     def __init__(self):
         self.states_sub = rospy.Subscriber('/pololu/motor_states', MSL, self.listen)
@@ -69,6 +75,13 @@ class SteeringDecider:
         self.dist_L = []
         self.dist_R = []
         self.oldest = 0
+        self.minmove_L = []
+        self.minmove_R = []
+        self.minmove_old = 0
+        self.coast_speed = 0.0
+        self.min_speed = 0.0
+        self.max_speed = 0.0
+        self.throttle = 0.0
 
     def listen(self, msg):
         # message can have multiple motor states
@@ -83,13 +96,15 @@ class SteeringDecider:
 	            self.dist_R.append(float(state.pulse))
 	        else:
 	            self.dist_R[self.oldest] = float(state.pulse)
+            elif state.name == "Throttle":
+                self.throttle = float(state.pulse)
 	self.oldest = (self.oldest+1) % HISTORY
 	# algorithm for value that I think we should care about; may need sensativity tuning
         L = LeftIR2Distance(sum(self.dist_L)/len(self.dist_L))
         R = RightIR2Distance(sum(self.dist_R)/len(self.dist_R))
         value = (L-R)/(min(L,R)-BUFFER)
         # convert to -pi/4 to pi/4 turning radians
-        value = (1/(1+exp(STEERING_SENSITIVITY*value))-0.5)*pi/4
+        value = (1/(1+exp(STEERING_SENSITIVITY*value))-0.5)*pi/2
 	# publish
         self.steering_setpoint_pub.publish(0.0)
         self.steering_state_pub.publish(value)
@@ -102,15 +117,53 @@ class SteeringDecider:
         # * increase when sensors are medium and close
         # * base speed should be enough that sensors change when not equal
         # * max speed should be ???? maybe based on last base speed? (except nonlinear)
-        #if (L-R)/min(L,R) > 1:
-            # one sensor is more than double the other
-            # slow until just fast enough to still see change (nonlinear is tricky)
-            # last
-            #L1 = sum(self.dist_L[(i+self.oldest) % len(self.dist_L) for ]
-            #if 
-	value = 0.1*pi/4-abs(value)
+        #
+        # update minmove (make sure the car is still moving when we want low speed)
+        if len(self.minmove_L) < MINMOVE_ITERATIONS:
+            self.minmove_L.append(L)
+            self.minmove_R.append(R)
+        else:
+            self.minmove_L[self.minmove_old] = L
+            self.minmove_R[self.minmove_old] = R
+        self.minmove_old = (self.minmove_old + 1) % len(self.minmove_L)
+        # decel if close to wall
+        if L < KILL_SOLO or R < KILL_SOLO or (L < KILL_DUAL and R < KILL_DUAL):
+            self.coast_speed = self.min_speed
+        # check for low change situation
+        if max(abs(self.minmove_L[self.minmove_old]-L)/L,
+               abs(self.minmove_R[self.minmove_old]-R)/R) < MINMOVE:
+            # increase min speed until a move is first detected
+            if self.min_speed >= self.coast_speed:
+                self.min_speed = 1/(1.0+self.coast_speed)
+            # increase speed very very slowly
+            self.coast_speed += 1/(1.0+self.coast_speed)
+            #print("low change coast = %f" % self.coast_speed)
+        else:
+            # plenty of momentum, choose speed based on how 
+            # one sensor is much 1.6x the other
+            if (L-R)/min(L,R) > 0.6:
+                #print("%f %f %f" %(L,R,(L-R)/min(L,R)))
+                # one wall is over double other wall, set to min speed and decay
+                self.coast_speed = 0.99*min(self.coast_speed, self.min_speed)
+                #print("double coast = %f" % self.coast_speed)
+            else:
+                # increase as long as steering is stable
+                # decay to min speed otherwise
+	        conflict = pow(1-abs(4/pi*value),2)
+                self.coast_speed += (-abs(self.coast_speed-self.min_speed)*0.01*(1-conflict)
+                                     + conflict*(1/(1.0+self.coast_speed)
+                                                 +(self.max_speed-self.min_speed)*0.01))
+                #print("weighted coast = %f" % self.coast_speed)
+        # track max speed reached, decaying slowly over time
+        if self.coast_speed > self.max_speed:
+            self.max_speed = self.coast_speed
+        else:
+            self.max_speed = self.min_speed + 0.999*(self.max_speed-self.min_speed)
+        # set throttle
+        value = THROTTLE_SENSITIVITY * self.coast_speed # * (0.5+0.5*(pi/4-abs(value)))
         self.throttle_setpoint_pub.publish(value)
-	self.throttle_state_pub.publish(0)
+        self.throttle_state_pub.publish(0)
+        print(self.throttle)
 
 
 def main():
